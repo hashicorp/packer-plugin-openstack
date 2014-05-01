mnaser/packer

Adds security group support
Allows security groups to be specified in the template.

builder/openstack/builder.go
@@ -103,6 +103,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 			Name:        b.config.ImageName,
 			Flavor:      b.config.Flavor,
 			SourceImage: b.config.SourceImage,
+			SecurityGroup: b.config.SecurityGroup,
 		},
 		&common.StepConnectSSH{
 			SSHAddress:     SSHAddress(csp, b.config.SSHPort, b.config.IPPoolName, newFloatingIp),

hashicorp/packer-plugin-openstack

builder/openstack/builder.go
@@ -88,9 +88,10 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 			DebugKeyPath: fmt.Sprintf("os_%s.pem", b.config.PackerBuildName),
 		},
 		&StepRunSourceServer{
-			Name:        b.config.ImageName,
-			Flavor:      b.config.Flavor,
-			SourceImage: b.config.SourceImage,
+			Name:           b.config.ImageName,
+			Flavor:         b.config.Flavor,
+			SourceImage:    b.config.SourceImage,
+			SecurityGroups: b.config.SecurityGroups,
 		},
 		&StepAllocateIp{
 			FloatingIpPool: b.config.FloatingIpPool,